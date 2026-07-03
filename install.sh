a262b1b638eabe1d81d2df0dd636bfead9673e40
sgofdez <45689084+sgofdez@users.noreply.github.com>
2026-07-03 12:17:55 +0200

    Remove installation check for joe

--- install.sh
+++ install.sh
@@ -4,11 +4,3 @@ if ! command -v antigravity &> /dev/null; then
 else
     echo "Antigravity CLI ya está instalado."
 fi
-
-# Install joe
-if ! command -v joe &> /dev/null; then
-    echo "Instalando joe..."
-    sudo apt-get update && sudo apt-get install -y joe
-else
-    echo "joe ya está instalado."
-fi
